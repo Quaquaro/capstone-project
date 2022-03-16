4461253134708c9eeedc16fbf56e6259644372c5
Christoph Timm <quaquaro@gmail.com>
2022-03-16 14:29:29 +0100

    :white_check_mark: Update and pass test for multi form

--- src/pages/TrackGameFormPage.spec.js
+++ src/pages/TrackGameFormPage.spec.js
@@ -4,7 +4,6 @@ import { MemoryRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TrackGameFormPage from './TrackGameFormPage.js';
 import Theme from '../Theme.js';
-import { wait } from '@testing-library/user-event/dist/utils';
 
 Wrapper.propTypes = {
   children: PropTypes.object
@@ -20,58 +19,38 @@ function Wrapper({ children }) {
 
 describe('TrackGameFormPage', () => {
   it('Test the multi-step form', () => {
-    render(<TrackGameFormPage />, { wrapper: Wrapper });
+    render(<TrackGameFormPage />, {
+      wrapper: Wrapper
+    });
+
     const testData = {
       nameOfGame: 'Test Game',
       playerNameOne: 'Test Player',
-      scoreOne: 'Test Score'
+      scoreOne: '44'
     };
 
     expect(screen.getAllByRole('textbox').length).toBe(1);
     expect(screen.getByRole('button', { name: /continue/i })).toBeInTheDocument();
     userEvent.type(screen.getByText(/name Of Game/i), testData.nameOfGame);
     userEvent.click(screen.getByText(/continue/i));
-    wait(() => expect(screen.getAllByPlaceholderText(/name/i).length).toBe(8));
+    expect(screen.getAllByPlaceholderText(/name/i).length).toBe(4);
 
-    screen.debug;
+    userEvent.type(screen.getByText(/player one/i), testData.playerNameOne);
+    userEvent.type(screen.getAllByText(/score/i)[0], testData.scoreOne);
+    userEvent.click(screen.getByRole('button', { name: /confirm/i }));
   });
 
-  // it('should submit form data when all required inputs are filled', () => {
-  //   const handleTrackGame = jest.fn();
-  //   render(
-  //     <MemoryRouter>
-  //       <Theme>
-  //         <TrackGameFormPage onTrackGame={handleTrackGame} />
-  //       </Theme>
-  //     </MemoryRouter>
-  //   );
-  //   const nameOfGameInput = screen.getByText(/name of game/i);
-  //   const playernameInput = screen.getByText(/player one/i);
-  //   const scoreInput = screen.getByPlaceholderText('1');
-  //   const submitButton = screen.getByRole('button', { name: /confirm/i });
+  it('should not submit if input is empty', () => {
+    const handleTrackGame = jest.fn();
+    render(<TrackGameFormPage onTrackGame={handleTrackGame} />, { wrapper: Wrapper });
+    const nameOfGameInput = screen.getByText(/name of game/i);
 
-  //   userEvent.type(nameOfGameInput, 'Imperial Settlers');
-  //   userEvent.type(playernameInput, 'Max');
-  //   userEvent.type(scoreInput, '75');
-  //   userEvent.click(submitButton);
+    userEvent.type(screen.getByText(/name Of Game/i), 'Tirvial');
+    userEvent.click(screen.getByText(/continue/i));
+    userEvent.type(nameOfGameInput, 'Dominion');
+    const submitButton = screen.getByRole('button', { name: /confirm/i });
+    userEvent.click(submitButton);
 
-  //   expect(handleTrackGame).toHaveBeenCalledTimes(1);
-  // });
-  // it('should not submit if input is empty', () => {
-  //   const handleTrackGame = jest.fn();
-  //   render(
-  //     <MemoryRouter>
-  //       <Theme>
-  //         <TrackGameFormPage onTrackGame={handleTrackGame} />
-  //       </Theme>
-  //     </MemoryRouter>
-  //   );
-  //   const nameOfGameInput = screen.getByText(/name of game/i);
-  //   const submitButton = screen.getByRole('button', { name: /confirm/i });
-
-  //   userEvent.type(nameOfGameInput, 'Dominion');
-  //   userEvent.click(submitButton);
-
-  //   expect(handleTrackGame).not.toHaveBeenCalledTimes(0);
-  // });
+    expect(handleTrackGame).toHaveBeenCalledTimes(0);
+  });
 });
