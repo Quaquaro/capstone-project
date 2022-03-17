c3dc940f879c7498fad02c959144c252dbfc8caa
Christoph Timm <quaquaro@gmail.com>
2022-03-17 11:46:56 +0100

    :lipstick:cancel icon with lower stroke width

--- src/components/FormHeader.js
+++ src/components/FormHeader.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import logo from '../img/greenDice.svg';
-import cancel from '../img/circle-cross.svg';
+import cancel from '../img/circle-cross-thin.svg';
 
 export default function FormHeader() {
   const navigate = useNavigate();
