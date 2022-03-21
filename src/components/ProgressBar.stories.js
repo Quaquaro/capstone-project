2c99387f065ee4275326fda2899386e3b651525d
Christoph Timm <quaquaro@gmail.com>
2022-03-21 10:45:48 +0100

    :sparkles: Add progress bar component

---
+++ src/components/ProgressBar.stories.js
@@ -0,0 +1,9 @@
+import ProgressBar from './ProgressBar.js';
+export default {
+  title: 'Component/ProgressBar',
+  component: ProgressBar
+};
+const Template = (args) => <ProgressBar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
