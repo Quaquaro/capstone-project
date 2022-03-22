4da7f586bce1ffe66d29d7d6050e7623ce7ba784
Christoph Timm <quaquaro@gmail.com>
2022-03-22 15:25:34 +0100

    :white_check_mark: Update and pass test for tracked-games-list with memory-router

--- src/components/TrackedGamesList.spec.js
+++ src/components/TrackedGamesList.spec.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import TrackedGamesList from './TrackedGamesList.js';
 import Theme from '../Theme.js';
 
@@ -17,10 +18,13 @@ describe('TrackedGamesList', () => {
       }
     ];
     render(
-      <Theme>
-        <TrackedGamesList games={nameOfGame} />
-      </Theme>
+      <MemoryRouter>
+        <Theme>
+          <TrackedGamesList games={nameOfGame} />
+        </Theme>
+      </MemoryRouter>
     );
+
     expect(screen.getByText(/7wonders/i)).toBeInTheDocument();
     expect(screen.getByText(/gustav/i)).toBeInTheDocument();
     expect(screen.getByText(/5/i)).toBeInTheDocument();
