2aa788856773e99962c60f4b0ea5a772930a2e0e
Christoph Timm <quaquaro@gmail.com>
2022-03-22 21:17:47 +0100

    :sparkles: Add a custom hook for fetching data

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+axios.defaults.baseURL = 'https://api.boardgameatlas.com/api';
+
+const useAxios = ({ url, method, body = null, headers = null }) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = () => {
+    axios[method](url, JSON.parse(headers), JSON.parse(body))
+      .then((res) => {
+        setResponse(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [method, url, body, headers]);
+
+  return { response, error, loading };
+};
+export default useAxios;
