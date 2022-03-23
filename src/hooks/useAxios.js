2114a8e258a22f3472d3f068127a2962bbecf120
Christoph Timm <quaquaro@gmail.com>
2022-03-23 21:22:43 +0100

    :recycle: Refactor useAxios hook

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,27 +2,25 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 axios.defaults.baseURL = 'https://api.boardgameatlas.com';
 
-const useAxios = ({ url, method, body = null, headers = null }) => {
-  const [response, setResponse] = useState(null);
+const useAxios = (axiosParams) => {
+  const [response, setResponse] = useState(undefined);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(true);
 
-  const fetchData = () => {
-    axios[method](url, JSON.parse(headers), JSON.parse(body))
-      .then((res) => {
-        setResponse(res.data);
-      })
-      .catch((err) => {
-        setError(err);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+  const fetchData = async (params) => {
+    try {
+      const result = await axios.request(params);
+      setResponse(result.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
-    fetchData();
-  }, [method, url, body, headers]);
+    fetchData(axiosParams);
+  }, []);
 
   return { response, error, loading };
 };
